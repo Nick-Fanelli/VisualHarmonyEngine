6e252f1fb41fb71038ce800123a38c358363fad4
Nick <nickfanelli5@gmail.com>
2021-01-22 14:02:13 -0500

    Added in stb_image header file

---
+++ HarmonyEngine/src/Render/Texture.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLUT/glut.h>
+
+class Texture {
+
+public:
+    // static GLuint LoadGLFWTexture(const char* imagepath) {
+    //     GLuint textureID;
+    //     glGenTextures(1, &textureID);
+
+    //     glBindTexture(GL_TEXTURE_2D, textureID);
+
+    // }
+}
