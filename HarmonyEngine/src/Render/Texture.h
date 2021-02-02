a086a8b18f7815a8c912533529e8c404b5cc6b10
Nick <nickfanelli5@gmail.com>
2021-02-01 21:27:25 -0500

    Made a change to the texture class

--- HarmonyEngine/src/Render/Texture.h
+++ HarmonyEngine/src/Render/Texture.h
@@ -21,10 +21,10 @@ class Texture {
     int m_Width, m_Height;
 
 public: 
-    Texture(const char* filepath, int width, int height) : m_Filepath(filepath), m_Width(width), m_Height(height) {}
+    Texture(const char* filepath, int width, int height) 
+        : m_Filepath(filepath), m_Width(width), m_Height(height) {}
 
     void Initialize() {
-
         // Generate Texture on GPU
         glGenTextures(1, &m_TextureID);
         glBindTexture(GL_TEXTURE_2D, m_TextureID);
