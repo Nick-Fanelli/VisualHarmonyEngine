e52629813339399ca80744615653a9ea8cad4188
Nick-Fanelli <nickfanelli5@gmail.com>
2021-02-19 18:23:49 -0500

    feat: add in precompiled header for harmony

---
+++ HarmonyEngine/src/harmonypch.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <iostream>
+#include <functional>
+#include <algorithm>
+#include <memory>
+#include <thread>
+#include <utility>
+
+#include <string>
+#include <stack>
+#include <array>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
