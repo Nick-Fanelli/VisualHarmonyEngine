bcb76793cd8b6b5178c6afef2bd59c8b1c309b1f
Nick-Fanelli <nickfanelli5@gmail.com>
2021-04-07 14:19:40 -0400

    chore: add in some basic tests

--- HarmonyEditor/src/EditorScene.cpp
+++ HarmonyEditor/src/EditorScene.cpp
@@ -16,8 +16,9 @@ namespace HarmonyEditor {
     using namespace HarmonyEngine;
 
     static Hierarchy s_Hierarchy = Hierarchy();
-    static entt::entity s_Entity;
 
+    static entt::entity s_Entity;
+    static Transform* s_Transform;
     static QuadRenderer* s_QuadRenderer;
 
     void EditorScene::OnCreate(GameContext* gameContext) {
@@ -29,27 +30,34 @@ namespace HarmonyEditor {
         ImGuiLayer::Initialize(gameContext);
 
         s_Entity = CreateGameObject();
-        Entity::AddComponent<Transform>(this, s_Entity, glm::vec2(-0.5, -0.5));
+
+        s_Transform = &Entity::AddComponent<Transform>(this, s_Entity, glm::vec2(-0.5, -0.5));
         s_QuadRenderer = &Entity::AddComponent<QuadRenderer>(this, s_Entity, glm::vec4(1, 1, 1, 1));
 
         s_Hierarchy.AddGameObject(s_Entity);
+
+//        s_Entity = CreateGameObject();
+//        s_Transform = &Entity::AddComponent<Transform>(this, s_Entity, glm::vec2(-0.5, -0.5));
+//        s_QuadRenderer = &Entity::AddComponent<QuadRenderer>(this, s_Entity, glm::vec4(1, 1, 1, 1));
+//
+//        s_Hierarchy.AddGameObject(s_Entity);
     }
 
-//void ShowRendererStatistics() {
-//    ImGui::Begin("Renderer Statistics");
-//
-//    ImGui::Text("Batch Count: %zu", RendererStatistics::GetBatchCount());
-//    ImGui::Text("Vertex Count: %zu", RendererStatistics::GetVertexCount());
-//    ImGui::Text("Index Count: %zu", RendererStatistics::GetIndexCount());
-//
-//    ImGui::End();
-//}
-
     void EditorScene::Update(const float& deltaTime) {
-        if (Input::StandardInput.IsKeyUp(HARMONY_KEY_A)) Log::Info("A");
 
-        RenderLayer::RenderScene();
+//        s_Transform->Position.x += deltaTime;
+
+//        if(Input::StandardInput.IsKey(HARMONY_KEY_W))
+//            s_Transform->Position.y += s_PlayerSpeed * deltaTime;
+//        if(Input::StandardInput.IsKey(HARMONY_KEY_S))
+//            s_Transform->Position.y -= s_PlayerSpeed * deltaTime;
+//        if(Input::StandardInput.IsKey(HARMONY_KEY_A))
+//            s_Transform->Position.x -= s_PlayerSpeed * deltaTime;
+//        if(Input::StandardInput.IsKey(HARMONY_KEY_D))
+//            s_Transform->Position.x += s_PlayerSpeed * deltaTime;
+
         ImGuiLayer::RenderImGuiWindows();
+        RenderLayer::RenderScene();
     }
 
     void EditorScene::OnDestroy() {
