3d83a8c933ceded465c3e88a89bf6ac5ce99b138
Nick-Fanelli <nickfanelli5@gmail.com>
2021-04-15 14:35:16 -0400

    chore: continue work on dockspace

--- HarmonyEditor/src/Layers/ImGuiLayer.cpp
+++ HarmonyEditor/src/Layers/ImGuiLayer.cpp
@@ -108,54 +108,104 @@ namespace HarmonyEditor {
         ImGui::CreateContext();
         ImGuiIO& io = ImGui::GetIO();
 
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+        io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+        io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+
         ImFontConfig imFontConfig = ImFontConfig();
         imFontConfig.OversampleH = 3;
         imFontConfig.OversampleV = 3;
 
         io.Fonts->AddFontFromFileTTF("/System/Library/Fonts/Helvetica.ttc", 13, &imFontConfig);
 
-        io.DisplaySize = ImVec2((float) gameContextPtr->GetDisplay().GetWidth(),
-                                (float) gameContextPtr->GetDisplay().GetHeight());
-        io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
-//    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+        ImGuiStyle& style = ImGui::GetStyle();
+        if(io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+            style.WindowRounding = 0.0f;
+            style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+        }
+
+//        io.DisplaySize = ImVec2((float) gameContextPtr->GetDisplay().GetWidth(),
+//                                (float) gameContextPtr->GetDisplay().GetHeight());
 
         LoadSettings();
 
         ApplySelectedTheme();
 
 //    ImGui::StyleColorsDark();
-        ImGui::PushStyleVar(ImGuiStyleVar_FrameRounding, 4);
-        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0);
+//        ImGui::PushStyleVar(ImGuiStyleVar_FrameRounding, 4);
+//        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0);
 
         ImGui_ImplGlfw_InitForOpenGL(gameContextPtr->GetDisplay().GetWindowPointer(), true);
         ImGui_ImplOpenGL3_Init(glsl_version);
     }
 
+    static bool s_DockspaceOpen = true;
+
     static void DrawDockSpace() {
+
+        static bool dockingEnabled = true;
+        static bool optFullscreenPersistant = true;
+        bool optFullscreen = optFullscreenPersistant;
+        static ImGuiDockNodeFlags dockspaceFlags = ImGuiDockNodeFlags_None;
+
+        ImGuiWindowFlags windowFlags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+        if(optFullscreen) {
+            ImGuiViewport* viewport = ImGui::GetMainViewport();
+
+            ImGui::SetNextWindowPos(viewport->Pos);
+            ImGui::SetNextWindowSize(viewport->Size);
+            ImGui::SetNextWindowViewport(viewport->ID);
+
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+
+            windowFlags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+            windowFlags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+        }
+
+        if(dockspaceFlags & ImGuiDockNodeFlags_PassthruCentralNode)
+            windowFlags |= ImGuiWindowFlags_NoBackground;
+
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+        ImGui::Begin("Dockspace", &s_DockspaceOpen, windowFlags);
+        ImGui::PopStyleVar();
+
+        if(optFullscreen)
+            ImGui::PopStyleVar(2);
+
+        // Dockspace
+        ImGuiIO& io = ImGui::GetIO();
+        if(io.ConfigFlags & ImGuiConfigFlags_DockingEnable) {
+            ImGuiID dockspaceID = ImGui::GetID("Dockspace");
+            ImGui::DockSpace(dockspaceID, ImVec2(0.0f, 0.0f), dockspaceFlags);
+        }
+
+        ImGui::End();
+
 // Create MenuBar inside window only if not on Mac.
 //#ifdef __APPLE__
 //        int windowFlags = ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_NoTitleBar |
 //                          ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
 //                          ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
 //#else
-        int windowFlags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_NoTitleBar |
-                          ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
-                          ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-//#endif
-
-        ImGui::SetNextWindowPos(ImVec2(0.0f, 0.0f), ImGuiCond_Always);
-        ImGui::SetNextWindowSize(s_GameContextPtr->GetDisplay().GetImGuiSize());
-
-        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
-
-        static bool s_Show = true;
-
-        ImGui::Begin("Dockspace", &s_Show, windowFlags);
-        ImGui::PopStyleVar(3);
-        ImGui::DockSpace(ImGui::GetID("Dockspace"));
-        ImGui::End();
+//        int windowFlags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_NoTitleBar |
+//                          ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
+//                          ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+////#endif
+//
+//        ImGui::SetNextWindowPos(ImVec2(0.0f, 0.0f), ImGuiCond_Always);
+//        ImGui::SetNextWindowSize(s_GameContextPtr->GetDisplay().GetImGuiSize());
+//
+//        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+//        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+//        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
+//
+//        static bool s_Show = true;
+//
+//        ImGui::Begin("Dockspace", &s_Show, windowFlags);
+//        ImGui::PopStyleVar(3);
+//        ImGui::DockSpace(ImGui::GetID("Dockspace"));
+//        ImGui::End();
     }
 
     static void DrawSettingPanel() {
@@ -195,9 +245,19 @@ namespace HarmonyEditor {
 //    ImGui::End();
 
         // End ImGui Frame
+
+        ImGuiIO& io = ImGui::GetIO();
+        io.DisplaySize = s_GameContextPtr->GetDisplay().GetImGuiSize();
+
         ImGui::Render();
-        ImGui::UpdatePlatformWindows();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        if(io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+            GLFWwindow* previousContext = glfwGetCurrentContext();
+            ImGui::UpdatePlatformWindows();
+            ImGui::RenderPlatformWindowsDefault();
+            glfwMakeContextCurrent(previousContext);
+        }
     }
 
     void ImGuiLayer::CleanUp() {
